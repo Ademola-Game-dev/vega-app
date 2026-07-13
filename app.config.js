1d6c0e49f4d094247cc51b7972edfbd701e845f7
8man <sangwanhimanshu8443@gmail.com>
2026-07-13 22:20:59 +0530

    chore(config): bump version to 3.5.0

--- app.config.js
+++ app.config.js
@@ -103,7 +103,7 @@ module.exports = () => {
       autolinking: {exclude: ['expo-splash-screen']},
       plugins,
       slug: 'vega',
-      version: '3.4.5',
+      version: '3.5.0',
       userInterfaceStyle: 'dark',
       experiments: {
         reactCompiler: true,
@@ -115,7 +115,7 @@ module.exports = () => {
         minSdkVersion: 28,
         edgeToEdgeEnabled: true,
         package: PACKAGE_NAME,
-        versionCode: 177,
+        versionCode: 178,
         permissions: [
           'FOREGROUND_SERVICE',
           'FOREGROUND_SERVICE_DATA_SYNC',
